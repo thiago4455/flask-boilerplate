5492ec99449e9ac1b5442a3d6948c94f2b0c509e
Thiago Mattos <thiagobuenosairesmattos@gmail.com>
2020-02-25 11:07:55 -0300

    Change utils location

--- src/api/components/default.py
+++ src/api/components/default.py
@@ -1,8 +1,5 @@
 from flask_restful import Resource
 
-from src.api.utils.db_connection import connect
-
 class Default(Resource):
     def get(self):
-        print(connect('SELECT * from users'))
         return '/'
